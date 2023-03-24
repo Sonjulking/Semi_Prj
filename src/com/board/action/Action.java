fc3cf1ee904ebdcb2c11dc13fdb53be9d8a0868d
harim-000 <halimseok@gmail.com>
2023-03-24 21:53:15 +0900

    harim commit

--- src/com/board/action/Action.java
+++ src/com/board/action/Action.java
@@ -1,5 +1,10 @@
 package com.board.action;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public interface Action {
+
+	ActionForward execute(HttpServletRequest request, HttpServletResponse response);
 
 }
