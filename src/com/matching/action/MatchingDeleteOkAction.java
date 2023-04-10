f8eb77827d7222affeba2e3e159db0acb7e638ca
vkfks1693 <vkfks1693@Daum.net>
2023-04-11 01:07:53 +0900

    jeongmin

--- src/com/matching/action/MatchingDeleteOkAction.java
+++ src/com/matching/action/MatchingDeleteOkAction.java
@@ -1,12 +1,14 @@
 package com.matching.action;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.matching.model.MatchingDAO;
+import com.member.model.MemberDTO;
 import com.project.controller.Action;
 import com.project.controller.ActionForward;
 
@@ -16,14 +18,25 @@ public class MatchingDeleteOkAction implements Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, MessagingException, Exception {
 		// 세션으로 저장된 로그인 아이디로 DB 삭제
-		int 
-		
+		String member_id = request.getParameter("id").trim();
 		
 		MatchingDAO dao = MatchingDAO.getInstance();
 		
-		int check = dao.deleteMatching();
+		MemberDTO mdto = new MemberDTO();
 		
-		return null;
+		mdto.setMember_id(member_id);
+		
+		int check = dao.deleteMatching(mdto);
+		
+		PrintWriter out = response.getWriter();
+		
+		ActionForward forward = new ActionForward();
+		
+		forward.setRedirect(false);
+		
+		forward.setPath("matching/matching.jsp");
+		
+		return forward;
 	}
 
 }
